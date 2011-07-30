ee750390f5d495d8bcee708e9643359270ab573c
Eric Lupinacci <eric@smellup.net>
2011-07-30 14:52:13 +0000

    Début des tests du nouveau fichier paquet.xml. On ajoute pour chaque plugin du core : - le paquet.xml sous le nom _paquet.xml pour éviter une utilisation par défaut à la place de plugin.xml Il suffira de le renommer pour effectuer les tests. - les fichiers de langue nécessaire à la description et au slogan. - un fichier _paquet-migration qui contient les commandes svn pour migrer ou ajouter des fichiers. Ce fichier n'a plus d'autre intérêt que de lister les fichiers à renommer pour retrouver le fonctionnement des balises disparues : install, fonctions et options.

---
+++ lang/paquet-vertebres_de.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-vertebres
+// Langue: de
+// Date: 30-07-2011 15:01:39
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// V
+	'vertebres_description' => 'Wirbel ermöglichen SQL Tabellen zu lesen, indem einer Seite der Parameter &quot;table:&quot; vorangestellt wird: <code>spip.php?page=table:articles</code>',
+	'vertebres_slogan' => 'SQL Tabellen lesen',
+);
+?>
---
+++ lang/paquet-vertebres_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-vertebres
+// Langue: fr
+// Date: 30-07-2011 15:01:39
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// V
+	'vertebres_description' => 'Vertebres propose un moyen de lire une table SQL en indiquant
+	comme argument de page la table à lire, précédé
+	de &quot;table:&quot;, par exemple : <code>spip.php?page=table:articles</code>',
+	'vertebres_slogan' => 'Lecteur de tables SQL',
+);
+?>
