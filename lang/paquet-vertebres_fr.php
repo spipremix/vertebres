b31a516431bbe985cd9c9cfeaeb786004e7e2cfd
guy.cesaro <guy.cesaro@gmail.com>
2011-11-21 14:36:56 +0000

    afficher un exemple qui marche dans le descriptif

--- lang/paquet-vertebres_fr.php
+++ lang/paquet-vertebres_fr.php
@@ -14,7 +14,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // V
 	'vertebres_description' => 'Vertebres propose un moyen de lire une table SQL en indiquant
 	comme argument de page la table à lire, précédé
-	de &quot;table:&quot;, par exemple : <code>spip.php?page=table:articles</code>',
+	de &quot;table:&quot;, par exemple : <code>ecrire/?exec=vertebres&table=spip_articles</code>',
 	'vertebres_nom' => 'Vertèbres',
 	'vertebres_slogan' => 'Lecteur de tables SQL',
 );
