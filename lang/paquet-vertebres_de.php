6b3851a278964c9f8e568110d91424a1f6a62b47
salvatore <salvatore@rezo.net>
2012-01-05 08:09:32 +0000

    langues (paquet-vertebres)

--- lang/paquet-vertebres_de.php
+++ lang/paquet-vertebres_de.php
@@ -1,19 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-vertebres
-// Langue: de
-// Date: 03-08-2011 19:45:21
-// Items: 3
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// V
+	// V
 	'vertebres_description' => 'Wirbel ermöglichen SQL Tabellen zu lesen, indem einer Seite der Parameter &quot;table:&quot; vorangestellt wird: <code>spip.php?page=table:articles</code>',
 	'vertebres_nom' => 'Wirbel',
-	'vertebres_slogan' => 'SQL Tabellen lesen',
+	'vertebres_slogan' => 'SQL Tabellen lesen'
 );
+
 ?>
--- lang/paquet-vertebres_fr.php
+++ lang/paquet-vertebres_fr.php
@@ -1,21 +1,16 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-vertebres
-// Langue: fr
-// Date: 03-08-2011 19:45:21
-// Items: 3
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/vertebres/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// V
+	// V
 	'vertebres_description' => 'Vertebres propose un moyen de lire une table SQL en indiquant
 	comme argument de page la table à lire, précédé
 	de &quot;table:&quot;, par exemple : <code>ecrire/?exec=vertebres&table=spip_articles</code>',
 	'vertebres_nom' => 'Vertèbres',
-	'vertebres_slogan' => 'Lecteur de tables SQL',
+	'vertebres_slogan' => 'Lecteur de tables SQL'
 );
+
 ?>
