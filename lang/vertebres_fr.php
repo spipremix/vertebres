8f583a8e74bb09f6a6578e87fd64091be9e444e5
salvatore <salvatore@rezo.net>
2012-01-05 08:09:31 +0000

    langues (vertebres)

--- lang/vertebres_fr.php
+++ lang/vertebres_fr.php
@@ -1,10 +1,15 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/dump/lang/
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/vertebres/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	'nb_donnees' => '@nb@ enregistrements',
+	// 1
 	'1_donnee' => '1 enregistrement',
+
+	// N
+	'nb_donnees' => '@nb@ enregistrements'
 );
+
+?>
