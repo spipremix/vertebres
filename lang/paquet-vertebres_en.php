41ac0e00586185d650ee9ef0962c9856938ca933
salvatore <salvatore@rezo.net>
2012-01-06 12:30:06 +0000

    langues (paquet-vertebres)

---
+++ lang/paquet-vertebres_en.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vertebres_description' => 'Vertebrae provides a way to read a SQL table indicating
+ as an argument of the page the page to read, preceded
+"table", for example: <code>ecrire/?exec=vertebres&table=spip_articles</code>',
+	'vertebres_nom' => 'Vertebrae',
+	'vertebres_slogan' => 'SQL tables reader'
+);
+
+?>
---
+++ lang/paquet-vertebres_sk.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vertebres_description' => 'Vertebres ponúka spôsob na čítanie tabuľky SQL        
+             ako argument zadaním stránky na prečítanie tabľky, pred ktorou je
+	"table:", napríklad: <code>ecrire/?exec=vertebres&table=spip_articles</code>',
+	'vertebres_nom' => 'Vertèbres',
+	'vertebres_slogan' => 'Čítanie tabuliek SQL'
+);
+
+?>
