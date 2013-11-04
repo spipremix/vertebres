6f17e20f8a47e1f21620bc6ef9846a3a4f9345a1
ben.spip <ben@rezo.net>
2013-11-04 21:48:19 +0000

    langues

--- lang/paquet-vertebres_fr_fem.php
+++ lang/paquet-vertebres_fr_fem.php
@@ -1,6 +1,8 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/vertebres/lang/
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-vertebres?lang_cible=fr_fem
+// ** ne pas modifier le fichier **
+
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
