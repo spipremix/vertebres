7d2161fe2461317bc6dc7f16eac9bb687f372a27
xuacusk8 <xuacusk8@gmail.com>
2017-11-14 17:43:45 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ paquet-vertebres] Export depuis http://trad.spip.net de la langue ast

---
+++ lang/paquet-vertebres_ast.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-vertebres?lang_cible=ast
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vertebres_description' => 'Vértebres propon un mediu pa lleer una tabla SQL indicando como argumentu de páxina la tabla a lleer, precedía por &quot;table :&quot;, por casu: <code>ecrire/ ?exec=vertebres&table=spip_articles</code>',
+	'vertebres_nom' => 'Vértebres',
+	'vertebres_slogan' => 'Llector de tables SQL'
+);
