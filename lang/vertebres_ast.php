4f07c4dfcf95beed6ff9925d66519cff56405525
xuacusk8 <xuacusk8@gmail.com>
2017-11-14 17:43:44 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ vertebres] Export depuis http://trad.spip.net de la langue ast

---
+++ lang/vertebres_ast.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/vertebres?lang_cible=ast
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// 1
+	'1_donnee' => '1 rexistru',
+
+	// N
+	'nb_donnees' => '@nb@ rexistros',
+
+	// T
+	'titre_tables' => 'Tables de la base'
+);
