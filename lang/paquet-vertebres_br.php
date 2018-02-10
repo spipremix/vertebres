a195198f38fbeb8d79c56ec9c1db0129dd59b48b
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-10 04:36:43 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ paquet-vertebres] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-vertebres_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-vertebres?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vertebres_description' => 'Melloù a ginnig deoc’h da lenn un daol SQL pa vez reiñ d’ar bajenn un arguzenn deus an daol da lenn, a zont goude &quot;table :&quot;, da skouer : <code>ecrire/ ?exec=vertebres&table=spip_articles</code>',
+	'vertebres_nom' => 'Melloù',
+	'vertebres_slogan' => 'Plaker taolioù SQL'
+);
