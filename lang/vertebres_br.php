3fdce47ff8d2b5c9527d0a4860468d20976bc90b
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-12 04:36:17 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ vertebres] Export depuis http://trad.spip.net de la langue br

---
+++ lang/vertebres_br.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/vertebres?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// 1
+	'1_donnee' => '1 enrolladenn',
+
+	// N
+	'nb_donnees' => '@nb@ enrolladennoù',
+
+	// T
+	'titre_tables' => 'Taolioù an diaz'
+);
