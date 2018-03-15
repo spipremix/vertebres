932fa99a0bf9da03b042d9e80dade07682bdd7c6
gpl <gpl@long-player.net>
2018-03-15 04:42:19 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ vertebres] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/vertebres_eo.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/vertebres?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// 1
+	'1_donnee' => '1 rikordo',
+
+	// N
+	'nb_donnees' => '@nb@ rikordoj',
+
+	// T
+	'titre_tables' => 'Tabeloj de la bazo'
+);
