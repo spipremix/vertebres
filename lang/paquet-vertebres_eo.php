bfa2f58fd348654ed25c71fbf4f8b4e58c39666a
gpl <gpl@long-player.net>
2018-03-15 04:42:19 +0000

    [Salvatore] [source:_core_/plugins/vertebres/lang/ paquet-vertebres] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-vertebres_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-vertebres?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vertebres_description' => 'Vertebroj proponas rimedon por legi SQL-tabelon, indikante kiel argumento de la paĝo la tabelon legotan, antaŭitan de „table=“. Ekzemple : <code>ecrire/?exec=vertebres&table=spip_articles</code>',
+	'vertebres_nom' => 'Vertebroj',
+	'vertebres_slogan' => 'SQL-tabela legilo'
+);
